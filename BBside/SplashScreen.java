038e4f769fc8dad1b97b8c8cadd6511a5176fd26
Martin Grabarczyk <Martin@Martin-Grabarczyks-MacBook-Pro.local>
2012-02-29 18:53:26 -0500

    this is a blackberry update

--- BBside/SplashScreen.java
+++ BBside/SplashScreen.java
@@ -1,6 +1,14 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
+
 import net.rim.device.api.ui.*;
 import net.rim.device.api.ui.component.*;
 import net.rim.device.api.ui.container.*;
+import net.rim.device.api.io.IOUtilities;
 import net.rim.device.api.system.Bitmap;
 import net.rim.device.api.system.PersistentObject;
 import net.rim.device.api.system.PersistentStore;
@@ -11,7 +19,8 @@ public class SplashScreen extends MainScreen implements FieldChangeListener {
     private EditField usernameField;
     private ButtonField loginButton;
     private ButtonField exitButton;
-
+    private StringBuffer sb1 = null;
+	private FileConnection fconn = null;
 
     public SplashScreen() {
     	   
@@ -40,13 +49,45 @@ public class SplashScreen extends MainScreen implements FieldChangeListener {
 
 
        public void fieldChanged(Field field, int context) {
-    	   	  //if quit button is pressed exit
-              if (field == loginButton) {
-            	  if (usernameField.getText().compareTo("test") == 0){
-            		  MainmenuScreen mms = new MainmenuScreen();
-                      UiApplication.getUiApplication().pushScreen(mms); 
-                      this.close();
-            	  }
+    	   	  //if login button is pressed search text file for username
+              if (field == loginButton) {        	  
+            	  
+				try {
+					fconn = (FileConnection)Connector.open("file:///SDCard/BlackBerry/filename.txt", Connector.READ_WRITE);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+            	  	if (fconn.exists())
+            	  	{
+            	  		InputStream input = null;
+						try {
+							input = fconn.openDataInputStream();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+            	  		sb1 = new StringBuffer();
+            	  		int chars,i=0;
+            	  		try {
+							while((chars=input.read())!=-1)
+							{
+								if ((chars=input.read())==32){
+									String str=sb1.toString();
+									if (usernameField.getText().compareTo(str) == 0){
+				            	  		MainmenuScreen mms = new MainmenuScreen();
+				            	  		UiApplication.getUiApplication().pushScreen(mms); 
+				            	  		this.close();
+				            	  	}
+									else 
+				            	  		sb1 = new StringBuffer();
+								}
+								else
+									sb1.append((char)chars);
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+            	  	}
               }
     	   	  //if quit button is pressed exit
               if (field == exitButton) {
