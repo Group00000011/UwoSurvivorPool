a4624540dbe0d9478d5c8d7e994ce1f23c65ad13
Manor Freeman <mfreema8@uwo.ca>
2012-03-01 14:23:33 -0500

    changed player class and added persistence

---
+++ src/Player.java
@@ -0,0 +1,187 @@
+/**
+ * Player class creates a player in the Survivor Pool
+ * 
+ * @author Manor Freeman, Hazel Rivera, Martin Grabarczyk, Liam Corrigan, Jeff
+ *         Westaway, Delerina Hill
+ * 
+ */
+public class Player {
+	// Attributes
+	private String firstName;
+	private String lastName;
+	private String userID;
+	private RoundPick finalPick;
+	private int roundOfFinalPick;
+	private int score;
+	private RoundPick[] weeklyPicks;
+
+	/**
+	 * Constructor for objects of class Player that initializes player's first
+	 * name, last name,ID, and number of weekly picks
+	 * 
+	 * @param firstName
+	 *            Players first name
+	 * @param lastName
+	 *            Player's Last name
+	 * @param userID
+	 *            Player's user ID
+	 **/
+	public Player(String firstName, String lastName, String userID){
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.userID=userID;
+		this.finalPick=null;
+		this.roundOfFinalPick= 0;
+		this.score = 0;
+		this.weeklyPicks=null;
+	}
+	
+
+	// Accessor methods
+
+	/**
+	 * Gets the player's first name
+	 * 
+	 * @return the first name of the player
+	 */
+	public String getFirst() {
+		return this.firstName;
+	}
+
+	/**
+	 * Gets the player's last name
+	 * 
+	 * @return the last name of the player
+	 */
+	public String getLast() {
+		return this.lastName;
+	}
+
+	/**
+	 * Get the player's ID
+	 * 
+	 * @return the ID of the player
+	 */
+	public String getID() {
+		return userID;
+	}
+
+	/**
+	 * Get the player's pick for which contestant will win the game
+	 * 
+	 * @return the final pick of the player
+	 */
+	public RoundPick getFinal() {
+		return this.finalPick;
+	}
+
+	/**
+	 * Get the round that the player chose his/her final pick
+	 * 
+	 * @return the round of the final pick
+	 */
+	public int getRoundOfFinal() {
+		return this.roundOfFinalPick;
+	}
+
+	/**
+	 * Get the player's score
+	 * 
+	 * @return the score of the player
+	 */
+	public int getScore() {
+		return this.score;
+	}
+
+	/**
+	 * Gets the players RoundPick for a specified round
+	 * 
+	 * @param roundNum
+	 *            The round of the RoundPick
+	 * @return the RoundPick for the specified round if it has been made, else return null 
+	 */
+	public RoundPick getWeekPick(int roundNum) {
+		if(this.weeklyPicks.length >= roundNum)
+			return this.weeklyPicks[roundNum - 1];
+		else
+			return null;
+	}
+
+
+	// Mutator methods
+
+	/**
+	 * Set the player's score
+	 * 
+	 * @param score
+	 *            The updated score of the player
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	// Other methods
+
+	/**
+	 * Increments the player's score
+	 * 
+	 * @param incNum
+	 *            The amount that the score should be incremented
+	 */
+	public void incScore(int incNum) {
+		this.score = this.score + incNum;
+	}
+
+	/**
+	 * Decrements the player's score
+	 * 
+	 * @param decNum
+	 *            The amount that the score should be decremented
+	 */
+	public void decScore(int decNum) {
+		this.score = this.score - decNum;
+	}
+
+	/**
+	 * Creates RoundPick object for specified round
+	 * 
+	 * @param roundNum
+	 *            the round of the round pick
+	 * @param cont
+	 *            the contestant to be eliminated on the specified round
+	 */
+	public void makeRoundPick(int roundNum, Contestant cont) {
+		if(this.weeklyPicks==null){
+			this.weeklyPicks=new RoundPick[roundNum];
+			this.weeklyPicks[roundNum - 1] = new RoundPick(roundNum, cont);
+		}
+		else if(this.weeklyPicks.length >= roundNum)
+			this.weeklyPicks[roundNum - 1] = new RoundPick(roundNum, cont);
+		else{
+			RoundPick[] newWeekPicks=new RoundPick[roundNum];
+			for(int i=0;i<this.weeklyPicks.length; i++){
+				newWeekPicks[i]=this.weeklyPicks[i];
+			}
+			newWeekPicks[roundNum-1] = new RoundPick(roundNum, cont);
+			this.weeklyPicks=newWeekPicks;
+		}
+		
+	}
+
+	/**
+	 * Sets the final RoundPick of the player
+	 * 
+	 * @param cont
+	 *            the contestant the player thinks will win
+	 * @param currRound
+	 *            the round that the player chose the final round pick
+	 * @param numRounds
+	 *            the number of rounds in the game
+	 */
+	public void chooseWinner(Contestant cont, int currRound, int numRounds) {
+		this.finalPick = new RoundPick(numRounds, cont);
+		this.roundOfFinalPick = currRound;
+		this.makeRoundPick(numRounds - 1,cont);
+	}
+
+}
--- src/UWOSurvivorPoolAdmin_Group3.java
+++ src/UWOSurvivorPoolAdmin_Group3.java
@@ -1,8 +1,87 @@
 
 import java.awt.*;
+import java.io.*;
 
 
 public class UWOSurvivorPoolAdmin_Group3 {
+//	private Player[] players;
+//	private Contestant[] contestants;
+//	
+//	public void readPlayers(String fileName){
+//        try{
+//            DataInputStream input = new DataInputStream(new FileInputStream(fileName));
+//            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+//            String currPlayer = in.readLine(), firstName="", lastName="", userID="";
+//            Player currPlayerObj;
+//            Boolean first, last, ID;
+//            first=last=ID=false;
+//            while(currPlayer!=null){
+//            for(int i=0; i<currPlayer.length(); i++){
+//                char ch=currPlayer.charAt(i);
+//                while(!first){
+//                	if(ch=='+')
+//                		first=true;
+//                	else
+//                		firstName=firstName+ch;
+//               }//end of first name field
+//                while(!last && first){
+//                	if(ch=='+')
+//                		last=true;
+//                	else
+//                		lastName=lastName+ch;
+//               }//end of  last name field
+//                while(!ID && last){
+//                	if(ch=='+')
+//                		ID=true;
+//                	else
+//                		userID=userID+ch;
+//               }//end of first last name field
+//               currPlayerObj=new Player(firstName, lastName, userID);
+//               addPlayer(currPlayerObj);
+//            }//end of scanning through currplayer
+//            first=last=ID=false;
+//            firstName=lastName=userID="";
+//            currPlayer=in.readLine();
+//            }//end of currplayer
+//        }
+//        catch(IOException e){}//unharmful
+//	}
+//	
+//	
+//    
+//	public void addPlayer(Player p){
+//		
+//		if(this.players==null){
+//			this.players=new Player[1];
+//			this.players[0]= p;
+//		}
+//		else{
+//			Player[] newPlayers=new Player[this.players.length];
+//			for(int i=0;i<this.players.length; i++){
+//				newPlayers[i]=this.players[i];
+//			}
+//			newPlayers[this.players.length] = p ;
+//			this.players=newPlayers;
+//		}
+//	}
+//	
+//	public void writePlayers(String fileName){
+//        BufferedWriter bWr = null;
+//        
+//        try {
+//            
+//            bWr = new BufferedWriter(new FileWriter(fileName));
+//            for(int i=0;i<this.players.length;i++){
+//            	
+//            }
+//            //Start writing to the output stream
+//            bufferedWriter.write("Writing line one to file");
+//            bufferedWriter.newLine();
+//            bufferedWriter.write("Writing line two to file");
+//	}
+//        
+//	}
+
 	public static void main(String[] args) 	{
 
 		SurvivorPoolAdminGUI gui = new SurvivorPoolAdminGUI();
